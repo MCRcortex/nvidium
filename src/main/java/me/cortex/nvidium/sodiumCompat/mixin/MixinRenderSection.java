5b8f95dc75a3b794ba7a889fc9b02e0c59669868
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2023-04-16 17:20:27 +1000

    Fixed animations

---
+++ src/main/java/me/cortex/nvidium/sodiumCompat/mixin/MixinRenderSection.java
@@ -0,0 +1,26 @@
+package me.cortex.nvidium.sodiumCompat.mixin;
+
+import me.cortex.nvidium.Nvidium;
+import me.jellysquid.mods.sodium.client.render.chunk.ChunkGraphicsState;
+import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
+import me.jellysquid.mods.sodium.client.render.chunk.data.ChunkRenderData;
+import me.jellysquid.mods.sodium.client.render.chunk.passes.BlockRenderPass;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(value = RenderSection.class, remap = false)
+public class MixinRenderSection {
+    @Redirect(method = "isEmpty", at = @At(value = "INVOKE", target = "Ljava/util/Map;isEmpty()Z"))
+    private boolean isEmpty(Map<BlockRenderPass, ChunkGraphicsState> instance) {
+        if (Nvidium.IS_ENABLED) {
+            return false;
+        }
+        return instance.isEmpty();
+    }
+}
