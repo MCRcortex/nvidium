b58ffec5cde45f85c8bb62b3879c2ccac3042bc6
mcrcortex <18544518+MCRcortex@users.noreply.github.com>
2023-04-24 15:44:30 +1000

    Added option to disable graph updates

--- src/main/java/me/cortex/nvidium/sodiumCompat/mixin/MixinSodiumWorldRenderer.java
+++ src/main/java/me/cortex/nvidium/sodiumCompat/mixin/MixinSodiumWorldRenderer.java
@@ -2,9 +2,13 @@ package me.cortex.nvidium.sodiumCompat.mixin;
 
 import me.cortex.nvidium.Nvidium;
 import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
+import me.jellysquid.mods.sodium.client.render.chunk.RenderSectionManager;
+import me.jellysquid.mods.sodium.client.util.frustum.Frustum;
+import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
@@ -19,6 +23,15 @@ public class MixinSodiumWorldRenderer {
             Nvidium.pipeline.addDebugInfo(debugStrings);
             cir.setReturnValue(debugStrings);
             cir.cancel();
+        }
+    }
+
+    @Redirect(method = "updateChunks", at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/render/chunk/RenderSectionManager;update(Lnet/minecraft/client/render/Camera;Lme/jellysquid/mods/sodium/client/util/frustum/Frustum;IZ)V"))
+    private void disableChunkUpdates(RenderSectionManager instance, Camera camera, Frustum frustum, int frame, boolean spectator) {
+        if (Nvidium.IS_ENABLED&&Nvidium.config.disable_graph_update) {
+            return;
+        } else {
+            instance.update(camera, frustum, frame, spectator);
         }
     }
 }
