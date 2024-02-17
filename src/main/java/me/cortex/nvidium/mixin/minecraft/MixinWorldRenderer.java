a2ff54a270f8513549976b478b9b9936d3cce54e
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-02-17 21:46:38 +0100

    Fixed the sky gradient being broken

--- src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWorldRenderer.java
+++ src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWorldRenderer.java
@@ -3,8 +3,10 @@ package me.cortex.nvidium.mixin.minecraft;
 import me.cortex.nvidium.Nvidium;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(WorldRenderer.class)
@@ -25,5 +27,10 @@ public class MixinWorldRenderer {
             return dist == 32 * 16 ? viewDistance : (dist == 256 * 16 ? 9999999 : dist);
         }
         return viewDistance;
+    }
+
+    @ModifyArg(method = "method_37365", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/BackgroundRenderer;applyFog(Lnet/minecraft/client/render/Camera;Lnet/minecraft/client/render/BackgroundRenderer$FogType;FZF)V"), index = 2)
+    private static float clampViewDistance(float viewDistance) {
+        return MathHelper.clamp(viewDistance, 2 * 16, 16 * 16);
     }
 }
