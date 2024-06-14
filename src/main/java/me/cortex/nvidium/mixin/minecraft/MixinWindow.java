127b6294d2e0d27158d135ac07b8776778882b03
Максим Темирбеков <77984877+GrinlexGH@users.noreply.github.com>
2024-06-14 10:12:35 +0300

    idk what i did but it works lol

--- src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWindow.java
+++ src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWindow.java
@@ -12,18 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Window.class)
 public class MixinWindow {
-    /*
-
-    Need to figure out why minecraft crashes with this method,
-    but it still doesn't do anything, so it can be disabled
-
-    @Inject(method = "<init>", at = @At(value = "HEAD"))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/lang/Object;<init>()V", shift = At.Shift.AFTER))
     private void headInit(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
         Nvidium.preWindowInit();
     }
-    */
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE_ASSIGN", target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;"))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;", shift = At.Shift.AFTER))
     private void init(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
         Nvidium.checkSystemIsCapable();
         Nvidium.setupGLDebugCallback();
